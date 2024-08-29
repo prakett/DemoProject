b653e5a141fb5462b1758289cc820b0972cfc4d4
praket <praketraj@gmail.com>
2024-08-29 09:35:30 +0530

    2nd commit

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def pro():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
